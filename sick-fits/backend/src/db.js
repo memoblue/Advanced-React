73982b1b3f32e6ce01e513f2466647b1fca1e63b
memo <me@memo.blue>
2019-05-14 08:28:20 -0700

    setup Yoga GraphQL

---
+++ sick-fits/backend/src/db.js
@@ -0,0 +1,12 @@
+// connect to Prisma
+require("dotenv").config({ path: "variables.env" });
+const { Prisma } = require("prisma-binding");
+
+const db = new Prisma({
+  typeDefs: "src/generated/prisma.graphql",
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false,
+});
+
+module.exports = db;
--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,16 @@
-// let's go!
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  deets => {
+    console.log(`serveis running on port http://localhost:${deets.port}`);
+  }
+);
